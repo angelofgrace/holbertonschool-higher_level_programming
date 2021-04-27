5bd356c2c7703e1890cb903ecc62e2d8a899958c
Grace Fallon <graceleefallon@gmail.com>
2021-04-27 19:02:26 +0000

    pep8 style update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,10 +11,11 @@ else:
     dig = number % 10
 if dig > 5:
     print("Last digit of" " {:d}"
-    .format(number) + " is" +  " {:d}".format(dig) + " and is greater than 5")
+          .format(number) + " is" + " {:d}"
+          .format(dig) + " and is greater than 5")
 elif dig < 6 and dig != 0:
     print("Last digit of" " {:d}".format(number) + " is" + " {:d}"
-    .format(dig) + " and is less than 6 and not 0")
+          .format(dig) + " and is less than 6 and not 0")
 else:
     print("Last digit of" + " {:d}"
-    .format(number) + " is" + " {:d}".format(dig) + " and is 0")
+          .format(number) + " is" + " {:d}".format(dig) + " and is 0")
