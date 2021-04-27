e3b53ab0885add3d35bbd23f64167114f71e7de0
Grace Fallon <graceleefallon@gmail.com>
2021-04-27 23:47:36 +0000

    Include args count statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{:d} arguments.".format(0))
-    else:
+    if len(sys.argv) > 1:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         i = 0
         for x in sys.argv:
             if i > 0:
                 print("{:d}: {:s}".format(i, x))
             i += 1
+    else:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
