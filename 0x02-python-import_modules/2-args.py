0f6ffbf52c813c5dd3d2d5d90393e11fed132f86
Grace Fallon <graceleefallon@gmail.com>
2021-04-27 23:54:53 +0000

    Account for singular arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
+    i = 0
+    if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
-        i = 0
-        for x in sys.argv:
-            if i > 0:
-                print("{:d}: {:s}".format(i, x))
-            i += 1
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
     else:
         print("{:d} arguments.".format(len(sys.argv) - 1))
+    for x in sys.argv:
+        if i > 0:
+            print("{:d}: {:s}".format(i, x))
+        i += 1
