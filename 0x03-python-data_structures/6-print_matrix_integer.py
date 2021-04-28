bd643b01a1b934660136ee4292127421eb8157f7
Grace Fallon <graceleefallon@gmail.com>
2021-04-28 20:26:43 +0000

    move edge cases into elif statement at top of code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for x in range(0, len(matrix[i])):
-            if x == (len(matrix[i]) - 1):
-                print("{:d}".format(matrix[i][x]))
-            else:
-                print("{:d}".format(matrix[i][x]), end=" ")
-    if len(matrix) <= 1:
-        print("")
     if not matrix:
         print("")
+    elif len(matrix) <= 1:
+        print("")
+    else:
+        for i in range(0, len(matrix)):
+            for x in range(0, len(matrix[i])):
+                if x == (len(matrix[i]) - 1):
+                    print("{:d}".format(matrix[i][x]))
+                else:
+                    print("{:d}".format(matrix[i][x]), end=" ")
