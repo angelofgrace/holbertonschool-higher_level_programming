b8ae77ed9b166dd505fa08699213e04e16e49dd9
Grace Fallon <graceleefallon@gmail.com>
2021-05-04 21:37:49 +0000

    order error checks appropriately to account for digits cast as strings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """ Initialization of private size variable """
         self.__size = size
-        if size < 0:
-            raise ValueError("size must be >= 0")
         if type(size) is not int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
