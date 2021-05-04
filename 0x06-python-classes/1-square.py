05265dd2458ffbb189a7b7a49fbeecbaa7569e5e
Grace Fallon <graceleefallon@gmail.com>
2021-05-04 21:08:57 +0000

    Size matters in a classy square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Store some shapes """
+
+
+class Square:
+    """ A classy square """
+    def __init__(self, size):
+        """ Initialization of private size variable """
+        self.__size = size
+
+
+
