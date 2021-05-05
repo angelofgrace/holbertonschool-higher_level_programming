a6ee11e688b732dfda692ce704c9b77cb64f6f15
Grace Fallon <graceleefallon@gmail.com>
2021-05-05 22:20:48 +0000

    Correctly implement OOP in perimeter method

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,6 +52,6 @@ class Rectangle:
 
     def perimeter(self):
         """ Find the Perimeter of a Rectangle object """
-        if __width == 0 or __height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
