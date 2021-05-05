81a0315679ed447cef382c65640b09eaadc7e2ef
Grace Fallon <graceleefallon@gmail.com>
2021-05-05 21:22:24 +0000

    Establish private attributes width and height and assoc. getter/setter fncs

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Creating classes of shapes """
+
+
+class Rectangle:
+    """ A class of Rectangles """
+
+    def __init__(self, width=0, height=0):
+        """ Istantiation of Rectangle object with optional height and width """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ Getter function for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter function for width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter function for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter function for width """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
