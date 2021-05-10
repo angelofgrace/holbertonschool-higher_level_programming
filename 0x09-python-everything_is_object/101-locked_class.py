2d048488bb56e34baf6dec495a10b45ef5e5d98f
Grace Fallon <graceleefallon@gmail.com>
2021-05-10 20:37:52 +0000

    pep8 stylings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,10 +5,11 @@
 class LockedClass:
     """ Class for names that prevents new attribute creation"""
 
-    __allowed_attributes=('first_name')
+    __allowed_attributes = ('first_name')
 
     def __setattr__(self, attribute, value):
-        if not attribute in self.__allowed_attributes:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attribute))
+        if attribute not in self.__allowed_attributes:
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(attribute))
         else:
             super().__setattr__(attribute, value)
