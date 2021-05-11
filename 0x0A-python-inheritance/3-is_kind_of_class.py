1daa4dfdd17b5c8d5375ae3c47fe9aa05b2ffe2f
Grace Fallon <graceleefallon@gmail.com>
2021-05-11 19:17:12 +0000

    Fix prototype error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 """ Check if an object is an instance of or inherits from a class """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ Is input object instance of or inheritor of input a_class """
     return isinstance(obj, a_class)
