b4fa144624f670c0a97db88fa7cd4ad336cea99f
Grace Fallon <graceleefallon@gmail.com>
2021-05-11 20:44:13 +0000

    Import BaseGeometry to avoid conflicts with testfiles and checker

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,18 +2,7 @@
 """ A little bit of geometrics """
 
 
-class BaseGeometry:
-    """ The basic elements of geometry """
-    def area(self):
-        """ Return an exception for some reason """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Ensure input value is an integer """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ A Geometric quadrilateral with two pairs of even sides """
