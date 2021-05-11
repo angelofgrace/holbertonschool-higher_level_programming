e6ef622b8e06cfb09a5bab80d9d214e637b15e65
Grace Fallon <graceleefallon@gmail.com>
2021-05-11 23:05:08 +0000

    We're entering the Matrix, tbh

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" What is equal is no longer so """
+
+
+class MyInt:
+    """ Switching it up, up in here """
+
+    def __init__(self, value):
+        """ Something to compare yourself to """
+        self.value = value
+
+    def __eq__(self, other):
+        """ No longer equals """
+        if self.value is other:
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+        """ Suddenly it equals """
+        if self.value is not other:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return "{}".format(self.value)
