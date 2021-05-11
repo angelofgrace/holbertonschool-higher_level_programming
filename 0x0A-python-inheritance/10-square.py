1655a8d158406b955a96156221974ff677dc2d52
Grace Fallon <graceleefallon@gmail.com>
2021-05-11 21:54:53 +0000

    Re-write __str__ for clarity

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,10 +9,16 @@ class Square(Rectangle):
     """ Rectangle where all sides are equal """
 
     def __init__(self, size):
+        """ Receive and validate size, the length of all sides """
         self.integer_validator("size", size)
         self.__width = size
         self.__height = size
         self.__size = size
 
     def area(self):
+        """ Area of a square """
         return self.__size ** 2
+
+    def __str__(self):
+        """ A simpler print string """
+        return "[Square] {}/{}".format(self.__size, self.__size)
