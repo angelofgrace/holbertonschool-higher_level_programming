2e82a6fe5f55109f68b01de6c68207f8321ffb47
Grace Fallon <graceleefallon@gmail.com>
2021-05-12 16:05:37 +0000

    Write a given string to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Input/Output task 1: write a file to stdout """
+
+
+def write_file(filename="", text=""):
+    """ Write a given string to a writable file and return # chars written """
+    with open(filename, mode="w", encoding="utf-8") as WriteMe:
+         return WriteMe.write(text)
