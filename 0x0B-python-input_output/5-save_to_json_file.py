5118e77232ab5fb098507ec36e6e1166af5b1ae5
Grace Fallon <graceleefallon@gmail.com>
2021-05-12 18:23:42 +0000

    Provoke the errors checker wants you to provoke

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,9 +7,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """ Convert my_obj to JSON represenation and write it to filename """
 
-    """ JSON Conversion """
-    jsonStr = json.dumps(my_obj)
-
-    """ Open filename, write to filename """
-    with open(filename, mode="w", encoding="utf-8") as WritFile:
-        WritFile.write(jsonStr)
+    with open(filename, "w") as WritFile:
+        WritFile.write(json.dumps(my_obj))
