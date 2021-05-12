ac11ba7600d2ab4ac0fa14bac9bbe743d408cf3e
Grace Fallon <graceleefallon@gmail.com>
2021-05-12 15:59:28 +0000

    Documentation and pep8 styling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" """
+""" Input/Output excercise 0: read a file """
 
 
 def read_file(filename=""):
+    """ Open a readable file and print it to stdout """
+
     with open(filename, mode="r", encoding="utf-8") as theFile:
         print(theFile.read(), end="")
