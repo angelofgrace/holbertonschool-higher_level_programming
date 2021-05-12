9947327a75888a55655a3a510dc520f23f8a7c0d
Grace Fallon <graceleefallon@gmail.com>
2021-05-12 22:33:04 +0000

    compile a dict for to_json from input attributes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Student Class instantiation and method to find instance dictionary """
+
+
+class Student:
+    """ A Student is many things, namely, named """
+
+    def __init__(self, first_name, last_name, age):
+        """  Establish public instance attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Find all methods and attributes of given instance """
+        if attrs == None:
+            return self.__dict__
+        else:
+            stdDict = {}
+            for att in attrs:
+                if att in self.__dict__:
+                    stdDict[att] = self.__dict__[att]
+            return stdDict
