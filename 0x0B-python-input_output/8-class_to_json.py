b29d1ebd9ac6c6b77b1861e7325633a80f63bee1
Grace Fallon <graceleefallon@gmail.com>
2021-05-12 21:45:46 +0000

    pep8 styling

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Return the dictionary description of a class for JSON obj serialization """
 
+
 def class_to_json(obj):
     """ Return dict description of obj class instance for JSON serialzation """
     return obj.__dict__
