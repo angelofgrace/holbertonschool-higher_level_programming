26cbd02c4f09a9e23f9d59596eb69351df2c14f4
Grace Fallon <graceleefallon@gmail.com>
2021-05-14 16:11:32 +0000

    Documentation and pep8 styling

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" """
+""" Foundation parent class for shapes """
 
 
 class Base:
-    """ """
+    """ Base shape class, instantiation, methods, attributes """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ """
+        """ Instantiation, id optional, iterating count as default id """
     if id is not None:
         self.id = id
     else:
