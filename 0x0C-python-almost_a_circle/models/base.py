481d46c865c7b1f5e241db89122728d3b5bcc08c
Grace Fallon <graceleefallon@gmail.com>
2021-05-14 16:17:33 +0000

    fix indentation error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,8 +9,8 @@ class Base:
 
     def __init__(self, id=None):
         """ Instantiation, id optional, iterating count as default id """
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
