b6f97e10a4f4738223b328fb22ad531aaf90d893
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 17:43:08 +0000

    Remove explicity declaration of foreign key, use simply, 'references'

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY (id) REFERENCES hbtn_0d_usa.states(id),
+    state_id INT NOT NULL REFERENCES hbtn_0d_usa.states (id),
     name VARCHAR(256) NOT NULL);
