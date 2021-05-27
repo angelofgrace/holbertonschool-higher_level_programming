76bd0c662daad89621fe28ea20331b38a20ace5a
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 14:46:45 +0000

    Condense script to accomodate checker expectations

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- list all priveleges of two MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
