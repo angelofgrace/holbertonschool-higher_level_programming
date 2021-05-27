a2e5373e2c44b6efaa62ea351f760a35625dafe1
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 02:56:58 +0000

    Use AS to alias the average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,6 @@
 -- compute the average score of all records
 ALTER TABLE second_table
 ADD average INT;
-INSERT INTO second_table (average)
+INSERT INTO second_table(average)
 VALUES
-    (SELECT AVG(score) FROM second_table);
+    (SELECT AVG(score) AS 'average' FROM second_table);
