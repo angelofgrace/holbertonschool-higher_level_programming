2bdc5c2bf88397680472581ad1a903b4c9cd218f
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 14:45:10 +0000

    Use simpler syntax, correctly refrence users by database

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list all priveleges of two MySQL users
-SELECT * FROM dba_sys_privs WHERE grantee = user_0d_1, user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
