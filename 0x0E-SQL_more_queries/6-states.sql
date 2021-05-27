ae09271318c1f70ac02b67d42ba3f9848a627cb4
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 16:46:14 +0000

    reference new table by parent database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create a database representing the usa.
 -- create a table of states, filled with names and ids.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL);
