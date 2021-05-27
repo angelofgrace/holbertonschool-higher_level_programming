554af84379421e9056c815858eb8e88af5d08c4b
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 15:56:28 +0000

    simplify

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- create a database and a user with select privilege.
-IF DB_ID('hbtn_0d_2') IS NOT NULL
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
