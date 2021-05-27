e67d87eecd63749742add95e430bdff8ea7a3a31
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 03:01:09 +0000

    Remove excess paranthetical

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -3,4 +3,4 @@ ALTER TABLE second_table
 ADD average INT;
 INSERT INTO second_table(average)
 VALUES
-(SELECT AVG(score) AS average FROM second_table);
+SELECT AVG(score) AS average FROM second_table;
