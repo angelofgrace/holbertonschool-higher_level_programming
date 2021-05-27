521aa9fc72eb3a4dee6c9aca62905ee0073420d8
Grace Fallon <graceleefallon@gmail.com>
2021-05-27 16:27:58 +0000

    Existing syntax error; script to create db and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create a database representing the usa.
+-- create a table of states, filled with names and ids.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT IDENTITY PRIMARY KEY,
+    name VARCHAR(256) NOT NULL);
