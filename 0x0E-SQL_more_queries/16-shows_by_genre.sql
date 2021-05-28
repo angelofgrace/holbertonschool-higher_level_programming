607233910e17e332b5d232007f32124d094114d9
Grace Fallon <graceleefallon@gmail.com>
2021-05-28 03:24:35 +0000

    Double left join shows to display even those without assoc genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- script lists all shows and all genres linked to each
+SELECT title, name
+FROM tv_shows show
+LEFT JOIN tv_show_genres n ON show.id=n.show_id
+LEFT JOIN tv_genres genre ON n.genre_id=genre.id
+ORDER BY title, name ASC;
