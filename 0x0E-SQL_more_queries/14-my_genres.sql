792447956d505b26729c0d4b655bc9955af6d5e3
Grace Fallon <graceleefallon@gmail.com>
2021-05-28 03:10:17 +0000

    Remove table from references to column in field list

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- script uses database to list all genres of a given show
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres g
 INNER JOIN tv_show_genres tv ON g.id=tv.genre_id
 INNER JOIN tv_shows shows ON tv.show_id=shows.id 
