74d05bb8566c0f6e590ca96c67950edd3c0e0220
Grace Fallon <graceleefallon@gmail.com>
2021-05-28 03:09:24 +0000

    Multiple (inner) join tables together to isolate records assoc with one element

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- script uses database to list all genres of a given show
+SELECT tv_genres.name
+FROM tv_genres g
+INNER JOIN tv_show_genres tv ON g.id=tv.genre_id
+INNER JOIN tv_shows shows ON tv.show_id=shows.id 
+AND shows.title='Dexter'
+ORDER BY g.name ASC;
