20620a942642787c35341ce0be3efb167966af37
Grace Fallon <graceleefallon@gmail.com>
2021-05-28 00:34:23 +0000

    compare correct values state.id to state_id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- list all the elements of a table that reference another table's element
 SELECT id, name FROM cities
-WHERE id IN (SELECT id FROM states WHERE name = 'California')
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
