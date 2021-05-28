5cda1435d34c7ae0eaa95589a427238412a018bc
Grace Fallon <graceleefallon@gmail.com>
2021-05-28 00:32:10 +0000

    changes to references

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- list all the elements of a table that reference another table's element
-SELECT id, name FROM hbtn_0d_usa.cities
+SELECT id, name FROM cities
 WHERE id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
 ORDER BY id ASC;
