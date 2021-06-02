5d61731e11e1398baa3a0cf980bae3509e58ae0e
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:51:35 +0000

    Loop through an array of strings sent to console log

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// print 3 lines using an array of strings and a loop
+
+var array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
