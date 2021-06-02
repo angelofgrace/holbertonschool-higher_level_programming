0ab7d4d171584ad750c544e1784a31928b479844
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:42:59 +0000

    Missing semicolon

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // print the first cmd line arg converted to an integer if possible
 
 if (isNaN(process.argv[2])) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
 }
