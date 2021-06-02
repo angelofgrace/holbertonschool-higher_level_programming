421384fa8192f2447801136c0d7deb67c66bc114
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:27:07 +0000

    Incoporate null check and send to stderr

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 // print the value of the first cmd line arg passed
 
-console.log(process.argv[2]);
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+  exit();
+} else {
+  console.log('No argument');
+  exit(1);
+}
