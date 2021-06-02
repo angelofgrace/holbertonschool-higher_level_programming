7a8f8936f090adb5796520b2ed7b39b341e6b560
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:18:58 +0000

    It starts on two. A script to print command line args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// print a message based on the number of arguments passed into script
+if (process.argv.length === 2) {
+    console.log('No argument');
+    process.exit();
+} else if (process.argv.length === 3) {
+    console.log('Argument found');
+    process.exit();
+} else {
+    console.log('Arguments founds');
+    process.exit();
+}
+
