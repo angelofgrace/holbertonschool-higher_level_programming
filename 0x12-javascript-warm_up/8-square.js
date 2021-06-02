dad4fc0abaa23087df5cf7d2f64f73cac5f72945
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 19:09:30 +0000

    Print a square of a given size

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// print a square of a given size
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (len = 0; len < process.argv[2]; len++) {
+   let str = '';
+    for (wid = 0; wid < process.argv[2]; wid++) {
+      str = str.concat('X');
+    }
+  console.log(str); 
+  }
+}
