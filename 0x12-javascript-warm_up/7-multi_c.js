8a437be9df53df8c46639f970ac4e19e724a9a9b
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:59:09 +0000

    Make unchanging variables into const

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  let x = process.argv[2];
+  const x = process.argv[2];
   let i = 0;
   while (i < x) {
     console.log('C is fun');
