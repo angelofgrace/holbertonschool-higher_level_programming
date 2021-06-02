7dcc738938f73ea8cad7ca8419040b93dc3f20d5
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:21:14 +0000

    Semistandard edits

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // print a message based on the number of arguments passed into script
 if (process.argv.length === 2) {
-    console.log('No argument');
-    process.exit();
+  console.log('No argument');
+  process.exit();
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
-    process.exit();
+  console.log('Argument found');
+  process.exit();
 } else {
-    console.log('Arguments found');
-    process.exit();
+  console.log('Arguments found');
+  process.exit();
 }
-
