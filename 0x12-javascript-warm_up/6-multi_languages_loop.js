d0979b82a197636bcc51075a40cb2149273f35fd
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 18:53:32 +0000

    Use const for an unchanging array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // print 3 lines using an array of strings and a loop
 
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
