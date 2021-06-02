023293fdb56727df3603067ccbfcc95139ace93d
Grace Fallon <graceleefallon@gmail.com>
2021-06-02 17:37:40 +0000

    Semistandard variation fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//print 3 lines to the standard output
+// print 3 lines to the standard output
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
