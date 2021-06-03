53c75b5d1f8381f35210523afb18381400304620
Grace Fallon <graceleefallon@gmail.com>
2021-06-03 16:47:13 +0000

    Add value and null checks to rectangle definition

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// define a rectangle with valid attribute values
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      const rect = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
