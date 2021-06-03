fc57860203f99129b1223692e67b4c36af4daa05
Grace Fallon <graceleefallon@gmail.com>
2021-06-03 02:50:52 +0000

    Find the second largest values among a list of ints

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// search a list of integers for the second largest among them
+
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  let max = -Infinity, result = -Infinity;
+  let x = 2;
+  while (process.argv[x]) {
+    const num = parseInt(process.argv[x]);
+    if (num > max) {
+      [result, max] = [max, num];
+    } else if (num < max && num > result) {
+      result = num;
+    }
+    x++;
+  }
+  console.log(result);
+}
