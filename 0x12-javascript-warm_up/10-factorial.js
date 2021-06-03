73e4c03d1b1dfa7131e6eaa4a9b85defa38cbc9b
Grace Fallon <graceleefallon@gmail.com>
2021-06-03 02:21:56 +0000

    Use a recursive function to print a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// print a factorial using recursion
+
+function f (n) {
+  if (isNaN(process.argv[2]) || n === 1) {
+    return (1)
+  } else {
+    return ((n !== 1) ? n * f(n - 1) : 1);
+  }
+}
+
+console.log(f(process.argv[2]));
